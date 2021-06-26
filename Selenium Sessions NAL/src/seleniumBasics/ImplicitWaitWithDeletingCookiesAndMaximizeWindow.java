6b6715170d8232bbf3eb1f653296a3087cf6640c
Akshat Rajvaidya <akshat.rajvaidya09@gmail.com>
2021-06-26 15:17:42 +0530

    Program to understand how to add implicit wait, page load timeout, maximize browser window & delete all cookies (used Selenium 3.141.59 to resolve the warning of deprecated function for pageLoadTimeout() & implicitlyWait()).

--- Selenium Sessions NAL/src/seleniumBasics/ImplicitWaitWithDeletingCookiesAndMaximizeWindow.java
+++ Selenium Sessions NAL/src/seleniumBasics/ImplicitWaitWithDeletingCookiesAndMaximizeWindow.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ImplicitWaitWithDeletingCookiesAndMaximizeWindow {
 
-	@SuppressWarnings("deprecation")
 	public static void main(String[] args) throws InterruptedException {
 		
 		System.setProperty("webdriver.chrome.driver", "D:\\Data\\Study Material\\Java\\selenium browser drivers\\chromedriver.exe");
