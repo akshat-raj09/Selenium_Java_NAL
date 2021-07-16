a75c88c7e034058eaad8accd67f7e023b0e8a4b5
Akshat Rajvaidya <akshat.rajvaidya09@gmail.com>
2021-07-16 22:37:30 +0530

    Created RetryAnalyser.java & AnnotationTransformer.java inside com.testNG package so that any failed test case can be re-run automatically at the runtime for a maximum of 2 times till the test case passes. Also added AnnotationTransformer as a <listener> inside testng.xml.

---
+++ Selenium Sessions NAL/src/com/testNG/RetryAnalyser.java
@@ -0,0 +1,31 @@
+package com.testNG;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryAnalyser implements IRetryAnalyzer {
+
+	private final int failedTestRetryLimit = 2;
+	private static int counter = 0;
+
+	@Override
+	public boolean retry(ITestResult result) {
+
+		
+
+		if (result.isSuccess() == false) {						// Check if test not succeed
+			if (counter < failedTestRetryLimit) {				// Check if counter is still < failedTestRetryLimit
+				counter++;										// Increase the counter by 1
+				result.setStatus(ITestResult.FAILURE);			// Mark test as failed
+				return true;									// Tells TestNG to re-run the test
+			} 
+			else {
+				result.setStatus(ITestResult.FAILURE);			// If failedTestRetryLimit reached, mark test as failed
+			}
+		} else {
+			result.setStatus(ITestResult.SUCCESS);				// If test passes, marks it as passed
+		}
+		return false;
+	}
+
+}
