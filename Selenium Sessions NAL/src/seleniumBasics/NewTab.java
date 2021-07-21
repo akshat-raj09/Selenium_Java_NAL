bec530521d59802128ae3121443eac3cf250fdbc
Akshat Rajvaidya <akshat.rajvaidya09@gmail.com>
2021-07-22 00:04:17 +0530

    This program demonstrates how to open a link in new tab in same window & then close the new tab after some time & switch control back to the main tab by using 2 methods:-  1.)By using keys.chord() on a link    2.)By using JavascriptExecutor Class.

--- Selenium Sessions NAL/src/seleniumBasics/NewTab.java
+++ Selenium Sessions NAL/src/seleniumBasics/NewTab.java
@@ -4,6 +4,7 @@ import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -29,7 +30,7 @@ public class NewTab {
 		chrome.get("https://www.amazon.in/");
 		
 		// get the window handle of current window & store it in current tab variable
-		String currentTab = chrome.getWindowHandle();	
+		String mainTab = chrome.getWindowHandle();	
 		
 		// this will open the link identified by the xpath in new tab
 		String clickLink = Keys.chord(Keys.CONTROL, Keys.ENTER);
@@ -41,7 +42,7 @@ public class NewTab {
 		// iterate over all the tabs & switch to the tab other than the main tab
 		for(String tab : tabs) {
 			// switch to the new tab
-			if(tab.equalsIgnoreCase(currentTab) == false) {
+			if(tab.equalsIgnoreCase(mainTab) == false) {
 				Thread.sleep(1000);
 				chrome.switchTo().window(tab);
 				break;
@@ -55,7 +56,62 @@ public class NewTab {
 		
 		// switch to main tab after 1 second
 		Thread.sleep(1000);
-		chrome.switchTo().window(currentTab);
+		chrome.switchTo().window(mainTab);
+		
+		// print the text of amazon pay link on main tab
+		System.out.println(chrome.findElement(By.xpath("//a[contains(text(),'Amazon Pay') and @class='nav-a  ']")).getText());
+		
+		// open a new blank tab using JavaScriptExecutor. 
+		// By opening a new by this method control of driver is not switched to new blank tab.
+		JavascriptExecutor js = ((JavascriptExecutor)chrome);
+		js.executeScript("window.open();");
+		Thread.sleep(1000);
+		chrome.switchTo().window(mainTab);
+		Thread.sleep(2000);
+		
+		// Set newTabs of type String will store the window handles of all the currently opened windows
+		Set<String> newTabs = chrome.getWindowHandles();	
+
+		// iterate over all the tabs & switch to the tab other than the main tab
+		for(String tab : newTabs) {
+			// switch to the new tab
+			if(tab.equalsIgnoreCase(mainTab) == false) {
+				Thread.sleep(1000);
+				chrome.switchTo().window(tab);
+				break;
+			}
+
+		}
+		
+		// after waiting for 2 seconds close the new tab & switch control back to main tab
+		Thread.sleep(2000);
+		chrome.close();
+		chrome.switchTo().window(mainTab);
+		Thread.sleep(1000);
+		
+		// open google.com in a new tab using JavascriptExecutor
+		// By opening a new tab by this method control of driver is not switched to new tab.
+		js.executeScript("window.open('https://www.google.com','_blank');");
+		Thread.sleep(1000);
+		chrome.switchTo().window(mainTab);
+		// Set newTab of type String will store the window handles of all the currently opened windows
+		Set<String> newTab = chrome.getWindowHandles();	
+
+		// iterate over all the tabs & switch to the tab other than the main tab
+		for(String tab : newTab) {
+			// switch to the new tab
+			if(tab.equalsIgnoreCase(mainTab) == false) {
+				Thread.sleep(1000);
+				chrome.switchTo().window(tab);
+				break;
+			}
+
+		}
+
+		// after waiting for 2 seconds close the new tab & switch control back to main tab
+		Thread.sleep(2000);
+		chrome.close();
+		chrome.switchTo().window(mainTab);
 		
 		// print the text of amazon pay link on main tab
 		System.out.println(chrome.findElement(By.xpath("//a[contains(text(),'Amazon Pay') and @class='nav-a  ']")).getText());
@@ -63,7 +119,7 @@ public class NewTab {
 		// close the browser after 3 seconds
 		Thread.sleep(3000);
 		chrome.quit();
-		
+				
 	}
 
 }
