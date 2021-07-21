3feef75baef04947041bef4fb4e0902af3acc195
Akshat Rajvaidya <akshat.rajvaidya09@gmail.com>
2021-07-21 16:38:09 +0530

    Program to understand how to select value from a dropdown by using Select Class. This program demonstrates use of various methods to select value from dropdown.

--- Selenium Sessions NAL/src/seleniumBasics/SelectDropdown.java
+++ Selenium Sessions NAL/src/seleniumBasics/SelectDropdown.java
@@ -1,13 +1,16 @@
 package seleniumBasics;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class SelectDropdown {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		System.setProperty("webdriver.chrome.driver", "D:\\Data\\Study Material\\Java\\selenium browser drivers\\chromedriver.exe");		
 		WebDriver chrome = new ChromeDriver();
@@ -15,26 +18,39 @@ public class SelectDropdown {
 		// Maximizes the opened window		
 		chrome.manage().window().maximize();
 		
+		// open the URL
 		chrome.get("https://www.testandquiz.com/selenium/testing.html");
 		
-		// Prints the text of the hyperlink "This is a link"	
-		System.out.println("\n" + chrome.findElement(By.linkText("This is a link")).getText() + "\n");
-		
-		chrome.findElement(By.id("fname")).sendKeys("Akshat");
-		chrome.findElement(By.id("idOfButton")).click();
-		chrome.findElement(By.xpath("//*[@id=\"male\"]")).click();
-		chrome.findElement(By.className("Automation")).click();
-		
-		// Selecting a value from dropdown
+		// creating an object of Select class then using that object to selecting value from a dropdown
 		Select dropdown = new Select(chrome.findElement(By.id("testingDropdown")));
-		dropdown.selectByVisibleText("Database Testing");
 		
-		try {
-			Thread.sleep(6000);
-		} 
-		catch (InterruptedException e) {
-			System.out.println(e);
-		}		
+		// print true if this is a multi select dropdown
+		System.out.println("This is a multi select dropdown: " + dropdown.isMultiple());
+		
+		// select by visible text
+		dropdown.selectByVisibleText("Database Testing");
+		Thread.sleep(1500);
+				
+		// select by index: this a bit risky if the options are added/deleted then the index will change.
+		dropdown.selectByIndex(1);
+		Thread.sleep(1500);
+		
+		// select by value
+		dropdown.selectByValue("Manual");
+		Thread.sleep(1500);
+		
+		// select value from a dropdown without using Select Class
+		List<WebElement> options = dropdown.getOptions();
+		for(WebElement option : options) {
+			if(option.getText().equalsIgnoreCase("Database Testing")) {
+				option.click();
+				break;
+			}
+			
+		}
+		
+		// close the browser after 4 seconds
+		Thread.sleep(4000);		
 		chrome.quit();
 	}
 
